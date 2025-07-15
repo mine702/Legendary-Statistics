5dc90e256bd18f632adcf1595d0f6100a8c9badb
mine <mine702@naver.com>
2025-07-15 14:18:45 +0900

    오류 수정

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,1 @@
-export const envMode: 'production' | 'development' | 'test' = import.meta.env.MODE as 'production' | 'development' | 'test';
 export const requestURL = import.meta.env.VITE_REQUEST_URL;
