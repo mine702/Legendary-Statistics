2527a4525607de9431b4dfdff4e29e5787e1ad33
mine <mine702@naver.com>
2025-07-15 13:34:48 +0900

    오류 수정

--- frontend/src/util/hooks/useSearchParamState.ts
+++ frontend/src/util/hooks/useSearchParamState.ts
@@ -6,8 +6,8 @@ import {SetURLSearchParams} from "react-router";
  * 이 훅을 사용하면 쿼리스트링이 상태로 관리됨으로서 setState가 발생할 때 마다 쿼리스트링이 변화하고, 네비게이션 히스토리가 갱신되어
  * 브라우저의 뒤로가기, 앞으로가기 버튼을 사용할 수 있습니다.
  */
-export const useSearchParamState = <T>(searchParamList:[URLSearchParams, SetURLSearchParams]
-                                       , paramName:string, initialState: T) => {
+export const useSearchParamState = <T>(searchParamList: [URLSearchParams, SetURLSearchParams]
+  , paramName: string, initialState: T) => {
   const [urlSearchParams, setURLSearchParams] = searchParamList;
 
   const parseValue = (value: string | null, defaultValue: T): T => {
@@ -17,16 +17,16 @@ export const useSearchParamState = <T>(searchParamList:[URLSearchParams, SetURLS
     return value as T;
   };
 
-  const param = parseValue(urlSearchParams.get(paramName),initialState);
+  const param = parseValue(urlSearchParams.get(paramName), initialState);
   const [state, setState] = useState<T>(param);
-  
-  const setParam = (value:T) => {
+
+  const setParam = (value: T) => {
     setURLSearchParams((prev) => {
-      prev.set(paramName, value.toString());
+      prev.set(paramName, String(value));
       return prev;
     }, {replace: true});
     setState(value);
   }
-  
+
   return [state, setParam] as const;
 }
