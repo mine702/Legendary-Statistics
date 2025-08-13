e4c39684b7f701d903a7f91066dc4536f04f2c9e
mine <mine702@naver.com>
2025-08-13 10:07:34 +0900

    google adsense

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -61,6 +61,12 @@ function App() {
     }
   }, [location.pathname])
 
+  // 라우트 변경 시
+  useEffect(() => {
+    // @ts-ignore
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, [location.pathname]);
+
   return (
     <Routes>
       <Route path="/authorized/:provider" element={<AuthorizedByOAuth onSuccess={onSocialAuthSuccess}/>}/>
